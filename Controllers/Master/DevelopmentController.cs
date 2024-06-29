a302175a2251367cefb3f9a52c588c2421fc129c
Alex <alex@netspacenz.com>
2024-06-29 21:10:58 +1200

    test

--- Controllers/Master/DevelopmentController.cs
+++ Controllers/Master/DevelopmentController.cs
@@ -1,9 +1,9 @@
-﻿
-using hoistmt.Services;
+﻿using hoistmt.Services;
 using Microsoft.AspNetCore.Mvc;
-
 using hoistmt.Functions;
-
+using Microsoft.Extensions.Configuration;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
 
 namespace hoistmt.Controllers
 {
@@ -13,11 +13,16 @@ namespace hoistmt.Controllers
     {
         private readonly ITenantDbContextResolver<TenantDbContext> _tenantDbContextResolver;
         private readonly Credits _credits;
+        private readonly IConfiguration _configuration;
 
-        public DevelopmentController(ITenantDbContextResolver<TenantDbContext> tenantDbContextResolver, Credits credits)
+        public DevelopmentController(
+            ITenantDbContextResolver<TenantDbContext> tenantDbContextResolver, 
+            Credits credits,
+            IConfiguration configuration)
         {
             _tenantDbContextResolver = tenantDbContextResolver;
             _credits = credits;
+            _configuration = configuration;
         }
 
         [HttpGet("Customers")]
@@ -38,6 +43,55 @@ namespace hoistmt.Controllers
 
             var hasCredits = _credits.hasCredits(companyDb);
             return Ok(hasCredits);
+        }
+
+        [HttpGet("connection-string")]
+        public IActionResult GetConnectionString()
+        {
+            string connectionString;
+            if (string.IsNullOrEmpty(_configuration["COOKIE_DOMAIN"]))
+            {
+                connectionString = _configuration.GetConnectionString("masterConnectionLocal");
+            }
+            else
+            {
+                connectionString = _configuration.GetConnectionString("masterConnectionRemote");
+            }
+
+            return Ok(new { ConnectionString = connectionString });
+        }
+
+        [HttpGet("ping")]
+        public async Task<IActionResult> PingAddresses()
+        {
+            var pingResults = new List<object>();
+
+            string[] ipAddresses = { "10.0.0.10", "10.0.0.11" };
+            foreach (var ipAddress in ipAddresses)
+            {
+                var ping = new Ping();
+                try
+                {
+                    var reply = await ping.SendPingAsync(ipAddress);
+                    pingResults.Add(new
+                    {
+                        Address = ipAddress,
+                        Status = reply.Status.ToString(),
+                        RoundtripTime = reply.RoundtripTime
+                    });
+                }
+                catch (PingException ex)
+                {
+                    pingResults.Add(new
+                    {
+                        Address = ipAddress,
+                        Status = "Error",
+                        ErrorMessage = ex.Message
+                    });
+                }
+            }
+
+            return Ok(pingResults);
         }
     }
 }
