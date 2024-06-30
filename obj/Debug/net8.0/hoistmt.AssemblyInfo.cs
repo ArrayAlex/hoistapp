2f5085ab976cadb91a901beced3a7682dd6a2c6d
Alex <alex@netspacenz.com>
2024-06-30 21:10:24 +1200

    TEST

--- obj/Debug/net8.0/hoistmt.AssemblyInfo.cs
+++ obj/Debug/net8.0/hoistmt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hoistmt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbcd50257cb910839ff01d6a762985eca03dd96e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cbf86f332cef337945b546fe3cc12c6ae225db9")]
 [assembly: System.Reflection.AssemblyProductAttribute("hoistmt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hoistmt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
